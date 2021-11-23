ec2b02fed9e9cfb3741cc992d90e91ab942029a6
MaiaVictor <srvictormaia@gmail.com>
2021-11-23 18:44:30 -0300

    Fix id benchmark

--- benchmarks/idtree_fus.js
+++ benchmarks/idtree_fus.js
@@ -2,14 +2,14 @@ function main() {
 
   let slow = n => {
     if (n === 0) {
-      return true;
+      return x => x;
     } else {
       let rec = x => x(slow(n - 1));
-      return rec(x => x) && rec(x => x);
+      return (rec(x => x))(rec(x => x));
     }
   };
 
   return slow(28);
 };
 
-console.log(main());
+console.log(main()(0));
